e27769c9174d2fe917ca2980193eae25fee862ff
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-11-06 15:08:30 -0500

    11/6

--- textBasedGame/src/annieJoannaMinigame/JoannaRoom.java
+++ textBasedGame/src/annieJoannaMinigame/JoannaRoom.java
@@ -13,20 +13,24 @@ public class JoannaRoom extends NPCRoom {
 	
 	public void performAction(int direction) {
 		if(direction ==  4) {
-			
-			
+			CaveExplorer.print("You obtained the net, now you can play. Press 'p' to play");
+			if(direction == 5 ) {
+					//mini game
+				}
+			else {
+				System.out.println("You can only enter 'w', 'a', 's', or 'd' to move or you can press 'p' to play since you already have a net.");
+			}
 		}
 			
 		
-		if(direction == 5 ) {
-					//mini game
-				}
+		
 		else
 			CaveExplorer.print("That key does nothing.");
+		printValidMoves();
 	}
 	
 	public void printValidMoves() {
-		System.out.println("You can only enter 'w', 'a', 's', or 'd' to move or you can press 'p' to play or 'i' for instructions.");
+		System.out.println("You can only enter 'w', 'a', 's', or 'd' to move or you can press 'p' to play, but first you need a net so press 'n'");
 	}
 	
 	public String validMoves() {
