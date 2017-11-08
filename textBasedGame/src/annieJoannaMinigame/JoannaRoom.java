75495b8c9f7ec2f90651d9228b4343d9a993e8e0
joann <joann@DESKTOP-56VO8EV>
2017-11-07 23:03:54 -0500

    w

--- textBasedGame/src/annieJoannaMinigame/JoannaRoom.java
+++ textBasedGame/src/annieJoannaMinigame/JoannaRoom.java
@@ -13,16 +13,16 @@ public class JoannaRoom extends NPCRoom {
 	
 	
 	public void performAction(int direction) {
-		if(direction ==  4) {
-			CaveExplorer.print("You play the game like....."); //instructions for game
+		if(direction ==  4 && !fishchant.buyNet()) {
+			CaveExplorer.print("First you need to buy a net. Look for the shopkeeper in the (northwest). Once you have bought a net come back to learn how to play the game."); //instructions for game
 		}
+		if(direction ==  4 && fishchant.buyNet()) {
+			CaveExplorer.print("To win try to outnumber Kevin C. Cucumber's score as he races against you."); //instructions for game
+		}
+		
 			if(direction == 5 ) {
 					//mini game
 				}
-			if(direction ==6)
-			{
-				fishchant.buyNet(100);
-			}
 			
 
 		else {
@@ -32,8 +32,7 @@ public class JoannaRoom extends NPCRoom {
 	}
 	}
 	public void printValidMoves() {
-		System.out.println("You can only enter 'w', 'a', 's', or 'd' to move or you can press 'p' to play, but first make sure you have a net" 
-				+"'e' to talk to the Fishchant.");
+		System.out.println("You can only enter 'w', 'a', 's', or 'd' to move or you can press 'p' to play, but first make sure you have a net.");
 	}
 	
 	public String validMoves() {
