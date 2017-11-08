3253a35d7153bf5afc7d1fe90641909ea20ca2d2
jofigure <31863340+jofigure@users.noreply.github.com>
2017-11-07 23:57:48 -0500

    Update JoannaRoom.java

--- textBasedGame/src/annieJoannaMinigame/JoannaRoom.java
+++ textBasedGame/src/annieJoannaMinigame/JoannaRoom.java
@@ -11,15 +11,16 @@ public class JoannaRoom extends NPCRoom {
 		super(description);
 	}	 
 	
-	
+	   
 	public void performAction(int direction) {
-		if(direction ==  4 && !fishchant.buyNet()) {
+		if(direction ==  4 && ShopKeeper.isGotNet()==false) {
 			CaveExplorer.print("First you need to buy a net. Look for the shopkeeper in the (northwest). Once you have bought a net come back to learn how to play the game."); //instructions for game
 		}
-		if(direction ==  4 && fishchant.buyNet()) {
+		else {
+		if(direction ==  4 && ShopKeeper.isGotNet()==true) {
 			CaveExplorer.print("To win try to outnumber Kevin C. Cucumber's score as he races against you."); //instructions for game
 		}
-		
+		else {
 			if(direction == 5 ) {
 					//mini game
 				}
@@ -30,6 +31,8 @@ public class JoannaRoom extends NPCRoom {
 			CaveExplorer.print("That key does nothing.");
 		printValidMoves();
 	}
+		}
+		}
 	}
 	public void printValidMoves() {
 		System.out.println("You can only enter 'w', 'a', 's', or 'd' to move or you can press 'p' to play, but first make sure you have a net.");
