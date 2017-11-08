a12733bb06120e029905e36ffec8aa7b9b8fb7df
joann <joann@DESKTOP-56VO8EV>
2017-11-07 22:22:28 -0500

    r

--- textBasedGame/src/annieJoannaMinigame/ShopKeeper.java
+++ textBasedGame/src/annieJoannaMinigame/ShopKeeper.java
@@ -3,9 +3,21 @@ package annieJoannaMinigame;
 import caveExplorer.NPC;
 
 public class ShopKeeper extends NPC {
+	private boolean active;
+	private String activeDescription;
+	private String inactiveDescription;
 
 	public ShopKeeper() {
-		// TODO Auto-generated constructor stub
+		this.activeDescription = "There is a fish merchant here. Press 'e' to talk.";
+		this.inactiveDescription = "No new items in stock right now.";
+		active = true;
 	}
 
+	public String getSymbol() {
+		return "F";
+	}
+	 
+	public boolean buyNet() {
+		
+	}
 }
