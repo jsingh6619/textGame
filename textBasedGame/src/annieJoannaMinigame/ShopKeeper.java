d04d567b0abfa9cd5396941d2461e0b3e36b1825
joann <joann@DESKTOP-56VO8EV>
2017-11-07 22:35:08 -0500

    1

--- textBasedGame/src/annieJoannaMinigame/ShopKeeper.java
+++ textBasedGame/src/annieJoannaMinigame/ShopKeeper.java
@@ -11,13 +11,23 @@ public class ShopKeeper extends NPC {
 		this.activeDescription = "There is a fish merchant here. Press 'e' to talk.";
 		this.inactiveDescription = "No new items in stock right now.";
 		active = true;
+				 setPosition(2,2);
+	}
+	public String getDescription() {
+		return activeDescription;
 	}
 
+	public String getInactiveDescription() {
+		return inactiveDescription;
+	}
 	public String getSymbol() {
 		return "F";
 	}
-	 
+	public boolean isActive() {
+		return active;
+	}
 	public boolean buyNet() {
 		
 	}
+	
 }
