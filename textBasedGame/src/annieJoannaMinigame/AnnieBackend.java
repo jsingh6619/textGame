3d9cb5ca2f4d1b0eae21a3a911462da7f37cfb47
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-11-15 15:02:18 -0500

    /

--- textBasedGame/src/annieJoannaMinigame/AnnieBackend.java
+++ textBasedGame/src/annieJoannaMinigame/AnnieBackend.java
@@ -3,19 +3,23 @@ package annieJoannaMinigame;
 public class AnnieBackend implements JoannaSupport {
 
 	private AnnieSupport frontend;
-	private AnnieJoannaPlot[][] plots;
 	private AnnieJoannaPlot[] jellyfishPlots;
+	private AnnieJoannaPlot[][] plots;
 
 	public AnnieBackend(AnnieSupport frontend) {
 		this.frontend = frontend;
 		jellyfishPlots = new AnnieJoannaPlot[0];
 		plots = new AnnieJoannaPlot[5][5];
+		setUpPlots();
+	}
+
+	private void setUpPlots() {
 		for(int row = 0; row < plots.length; row++)
 			for(int col = 0; col < plots[row].length; col++)
 				if(row == 0 && col == 0 || row == plots.length - 1 && col == plots[row].length - 1)
 					plots[row][col] = new AnnieJoannaPlot(false);
 				else {
-					boolean jellyfishPresent = random();
+					boolean jellyfishPresent = Math.random() < .5;
 					plots[row][col] = new AnnieJoannaPlot(jellyfishPresent);
 					if(jellyfishPresent)
 						addJellyfishPlot(plots[row][col]);
@@ -28,12 +32,6 @@ public class AnnieBackend implements JoannaSupport {
 			temp[i] = jellyfishPlots[i];
 		temp[temp.length - 1] = plot;
 		jellyfishPlots = temp;
-	}
-
-	private boolean random() {
-		if(Math.random() < .5)
-			return true;
-		return false;
 	}
 
 	public AnnieJoannaPlot[] getJellyfishPlots() {
