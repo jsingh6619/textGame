503ef7ca15b083142a480de15dd17ffa1be97024
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-11-20 14:57:11 -0500

    added hand param to setCard

--- textBasedGame/src/abidAbedJasMinigame/AbedSupportBack.java
+++ textBasedGame/src/abidAbedJasMinigame/AbedSupportBack.java
@@ -11,7 +11,7 @@ public interface AbedSupportBack
 	int getCardRow();//returns where the card was on board Xcoordinate
 	int getCardCol();//returns where the card was on board Ycoordinate
 	AbidCard getLastCard();///returns the class
-	void setCard(int row, int col, int index);
+	void setCard(int row, int col, int index,AbidCard[] hand);
 
 
 }
