3151e89f9e0bfad0579220d68f48def9187c567e
a <a@localhost>
2017-11-23 15:36:18 -0500

    y

--- textBasedGame/src/abidAbedJasMinigame/AbidFrontend.java
+++ textBasedGame/src/abidAbedJasMinigame/AbidFrontend.java
@@ -22,7 +22,7 @@ public class AbidFrontend implements JasSupportFront , AbedSupportFront {
 	}
 	public AbidFrontend() {
 		this.backend = new JasBackend(this);
-		this.ai = new AbedAI(this);
+		this.ai = new AbedAI(this,null);
 		CaveExplorer.in = new Scanner(System.in);
 	}
 	
